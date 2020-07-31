17b7419fef1108a0a72666bb2102afa276085877
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2020-07-31 17:48:54 -0500

    Avance en calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,24 +9,32 @@ public class Calculadora {
 
         List n1 = new List();
         List n2 = new List();
+        List res = new List();
 
         System.out.println("Ingrese el primer numero mayor a treinta digitos");
         String n1_str = scan.nextLine();
-        for (int i = 0; i < n1_str.length() ; i++) {
-            n1.add((int)n1_str.charAt(i));
+        for (int i = n1_str.length()-1; i>=0; i--) {
+            n1.add(Character.getNumericValue(n1_str.charAt(i))); //agregar numero a la lista de antras hacia adelante
         }
 
         System.out.println("Ingrese el segundo numero mayor a treinta digitos");
         String n2_str = scan.nextLine();
-        for (int i = 0; i < n2_str.length() ; i++) {
-            n2.add((int)n2_str.charAt(i));
+        for (int i = n2_str.length()-1; i>=0; i--) {
+            n2.add(Character.getNumericValue(n2_str.charAt(i))); //agregar numero a la lista de antras hacia adelante
         }
 
+        //se agregan así con el fin de tener primero unidades, luego decenas, centenas, etc.
+        //ESTO SOLO SIRVE PARA SUMA RESTA MULTIPLICAION
+
+        //PARA DIVISION SE NECESITA OTRO ORDEN
+
         System.out.println("Que operación quiere realizar con estos? (Escriba +,-,*,/)");
+
 
         String g = scan.nextLine();
         switch (g){
             case "+":
+
                 break;
             case "-":
                 break;
